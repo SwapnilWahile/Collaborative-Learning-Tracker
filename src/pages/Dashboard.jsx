50c92f10b1552a16a69d2978f0221ec6d934a05b
SwapnilWahile <swapnilwahile24@gmail.com>
2025-07-23 21:08:12 +0530

    progress bar add in plan

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,22 @@
 import AddPlan from "../components/AddPlan";
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from 'react-redux';
-import { addPlan } from '../store/plansSlice';
+import { useSelector, useDispatch } from "react-redux";
+import { addPlan } from "../store/plansSlice";
 
 const Dashboard = () => {
-
-  const plans = useSelector(state => state.plans);
+  const plans = useSelector((state) => state.plans);
   const dispatch = useDispatch();
 
   const handleAddPlan = (plan) => {
     dispatch(addPlan(plan));
   };
+
+  const getProgress = (plan) => {
+    const total = plan.tasks.length;
+    const completed = plan.tasks.filter((t) => t.completed).length;
+    return total === 0 ? 0 : Math.round((completed / total) * 100);
+  };
+
   return (
     <div className="dashboard container py-4">
       <h2>My Study Plans</h2>
@@ -18,16 +24,36 @@ const Dashboard = () => {
 
       <div className="row mt-4">
         {plans.length === 0 && <p>No plans yet. Start by adding one!</p>}
-        {plans.map((plan, index) => (
-          <div key={index} className="col-md-4 mb-3">
-            <Link to={`/plan/${plan.id}`}>
-              <div className="card p-3 shadow-sm">
-                <h5>{plan.name}</h5>
-                <p>{plan.tasks.length} tasks</p>
-              </div>
-            </Link>
-          </div>
-        ))}
+        {plans.map((plan, index) => {
+          const progress = getProgress(plan);
+          return (
+            <div key={index} className="col-md-3 mb-3">
+              <Link to={`/plan/${plan.id}`}>
+                <div className="card p-3 shadow-sm">
+                  <div class="row" title={plan.name}>
+                    <h5 class="col-11 text-truncate">{plan.name}</h5>
+                  </div>
+                  <p>{plan.tasks.length} tasks</p>
+
+                  {plan.tasks.length !== 0 && (
+                    <div className="progress mb-2" style={{ height: "20px" }}>
+                      <div
+                        className="progress-bar bg-success"
+                        role="progressbar"
+                        style={{ width: `${progress}%` }}
+                        aria-valuenow={progress}
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        {progress}%
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
